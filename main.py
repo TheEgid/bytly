1695cb6466a3767cdb6f7edcf2937db327531ebf
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-05-02 09:11:49 +0300

    Make small planned changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import requests
 from dotenv import load_dotenv
 
+
 def make_bitlink(token, long_link):
     url = 'https://api-ssl.bitly.com//v4/bitlinks'
     headers = {'Authorization': 'Bearer ' + token}
@@ -14,14 +15,17 @@ def make_bitlink(token, long_link):
     else:
         return None
 
+
 def get_url_without_protokol(link):
     delimiter = '://'
     protocol = link.find(delimiter)
     return link.replace(link[0:protocol+len(delimiter)], '')
 
+
 def fetch_clicks(clicks_list):
     fetch_clicks_list = [x['clicks'] for x in clicks_list]
     return 'Количество переходов по ссылке битли: '+ str(sum(fetch_clicks_list))
+
 
 def extract_fetch_clicks(token, bitlink):
     url = 'https://api-ssl.bitly.com/v4/bitlinks/'
@@ -37,13 +41,14 @@ def extract_fetch_clicks(token, bitlink):
     else:
         return None
 
+
 def stage_bitly_cooperation(token, link):
     try:
         if link.find('bit.ly') > 0:
             return extract_fetch_clicks(token, link)
         else:
             return make_bitlink(token, link)
-    except():
+    except:
         return None
 
 
